7822cbfec7d52b870c135b0b228dae868f327726
Janika Hortizuela <jhortizuela21@gmail.com>
2023-09-11 12:56:10 -0600

    Implement carousel

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -12,52 +12,79 @@ import rancidtomatillostext from '../assets/rancidtomatillostext.png';
 import overlook from '../assets/overlook.png';
 import overlooktext from '../assets/overlooktext.png';
 import banner from '../assets/banner.png';
+import carouselarrow from '../assets/carousel-arrow.png';
+import projects from '../assets/projects.png'
 import { Link } from 'react-router-dom';
+import { responsive } from './responsive';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const Projects = () => {
+
+
+  const CustomRight = ({ onClick }) => (
+    <button className="arrow right" onClick={onClick}>
+      <img src={carouselarrow} />
+    </button>
+  );
+  const CustomLeft = ({ onClick }) => (
+    <button className='arrow left' onClick={onClick} >
+      <img src={carouselarrow} />
+    </button>
+  );
   return (
     <div className='projects'>
-      <img src={banner} alt='janika codes stuff'  className='banner'/>
+      <img src={banner} alt='janika codes stuff' className='banner' />
+    <img src={projects} alt='projects' />
       <Link to='/'>
-        <img src={goback} alt='go back'  className='goback'/>
+        <img src={goback} alt='go back' className='goback' />
       </Link>
-      <div className='projects-container'>
-        <section className='project'>
+
+      <Carousel
+        responsive={responsive}
+        swipeable={true}
+        arrows={true}
+        customRightArrow={<CustomRight />}
+        customLeftArrow={<CustomLeft />}
+      >
+        <div className='project'>
           <img src={twitterapi} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={twitterapitext} alt='link to twitter api' />
           </a>
-        </section>
-        <section className='project'>
+        </div>
+        <div className='project'>
           <img src={rosso} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={rossotext} alt='link to rosso' />
           </a>
-        </section>
-        <section className='project'>
+        </div>
+        <div className='project'>
           <img src={thumbtack} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={thumbtacktext} alt='link to rosso' />
           </a>
-        </section>
-        <section className='project'>
+        </div>
+        <div className='project'>
           <img src={novelidea} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={novelideatext} alt='link to rosso' />
           </a>
-        </section>
-        <section className='project'>
+        </div>
+        <div className='project'>
           <img src={rancidtomatillos} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={rancidtomatillostext} alt='link to rosso' />
           </a>
-        </section>
-        <section className='project'>
+        </div>
+        <div className='project'>
           <img src={overlook} />
           <a href='https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-7-2023-team-3'>
             <img src={overlooktext} alt='link to rosso' />
           </a>
-        </section>
-      </div>
+        </div>
+      </Carousel>
+      {/* </div> */}
     </div>
   );
 };
